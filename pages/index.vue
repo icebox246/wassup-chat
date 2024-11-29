4498274f6b84542316aace012a2c2ae2a7f0db75
icebox246 <iceboxpoczta@gmail.com>
2024-11-29 18:26:40 +0100

    Make fetching bug less aggressive

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,20 @@
   <UContainer class="flex flex-col items-center">
     <h1 class="text-5xl">WassupChat</h1>
     <h2 class="p-7">Chat with everyone</h2>
-    <div v-if="!store.loggedIn" class="grid grid-cols-2 gap-3">
-      <UButton @click="handleRegister" icon="i-mdi-add"> Sign up </UButton>
-      <UButton @click="handleLogin" icon="i-mdi-login" variant="outline"> Sign in </UButton>
+    <div v-if="store.loggedIn">
+      <div class="grid grid-cols-2 gap-3">
+        <UButton @click="handleRegister" icon="i-mdi-add"> Sign up </UButton>
+        <UButton @click="handleLogin" icon="i-mdi-login" variant="outline"> Sign in </UButton>
+      </div>
     </div>
-    <div v-else>
+    <div v-else class="grid-cols-1">
       <UButton @click="handleApp" icon="i-mdi-chat-processing"> Open app </UButton>
     </div>
   </UContainer>
 </template>
 
 <script lang="ts" setup>
-const store = useMyAppStore()  
+const store = useMyAppStore()
 
 function handleRegister() {
   navigateTo("/register");
