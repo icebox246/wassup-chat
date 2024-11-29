897ccfeb3d1eff5ce8378f884e3734ce88d4886b
icebox246 <iceboxpoczta@gmail.com>
2024-11-29 18:20:51 +0100

    Factor out getting current user

--- server/api/channel.post.ts
+++ server/api/channel.post.ts
@@ -1,38 +1,32 @@
 import prisma from '~/lib/prisma';
-import { getUserFromToken } from '~/utils';
 import type { Channel } from '@prisma/client'
+import { getCurrentUser } from '~/utils';
 
 export default defineEventHandler(async (event): Promise<{ channel?: Channel, err?: Error }> => {
-  const token = getCookie(event, 'auth-cookie')
-  if (!token) {
-    setResponseStatus(event, 403, "Forbidden");
-    return { err: Error("Not signed in") };
-  }
-  const user = await getUserFromToken(token)
-  if (!user) {
-    setResponseStatus(event, 401, "Forbidden");
-    return { err: Error("Invalid token") };
-  }
-
-  const { name, topic } = await readBody(event);
-
   try {
-    const channel = await prisma.channel.create({
-      data: {
-        name, topic,
-        admin: {
-          connect: user
-        },
-        members: {
-          connect: user
-        }
-      }
-    })
-    console.log("created a channel:", channel)
+    const user = await getCurrentUser(event);
+    const { name, topic } = await readBody(event);
 
-    return { channel }
+    try {
+      const channel = await prisma.channel.create({
+        data: {
+          name, topic,
+          admin: {
+            connect: user
+          },
+          members: {
+            connect: user
+          }
+        }
+      })
+      console.log("created a channel:", channel)
+
+      return { channel }
+    } catch (err) {
+      console.error("failed to create user:", err)
+      return { err: err as Error }
+    }
   } catch (err) {
-    console.error("failed to create user:", err)
     return { err: err as Error }
   }
 })
