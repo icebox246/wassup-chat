353b36fe6b10be1e27133a7be34cf87b476232c7
icebox246 <iceboxpoczta@gmail.com>
2024-12-06 10:36:34 +0100

    Slight change of button styles

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,12 @@
     <h2 class="p-7">Chat with everyone</h2>
     <div v-if="!(store.loggedIn && store.currentUser)">
       <div class="grid grid-cols-2 gap-3">
-        <UButton @click="handleRegister" icon="i-mdi-add"> Sign up </UButton>
-        <UButton @click="handleLogin" icon="i-mdi-login" variant="outline"> Sign in </UButton>
+        <UButton @click="handleRegister" icon="i-mdi-add" size="lg"> Sign up </UButton>
+        <UButton @click="handleLogin" icon="i-mdi-login" size="lg" variant="outline"> Sign in </UButton>
       </div>
     </div>
     <div v-else class="grid-cols-1">
-      <UButton @click="handleApp" icon="i-mdi-chat-processing"> Open app </UButton>
+      <UButton @click="handleApp" icon="i-mdi-chat-processing" size="xl"> Open app </UButton>
     </div>
   </UContainer>
 </template>
